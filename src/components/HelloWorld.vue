5369c64d1c3dbcbc1697a46bf46d6477b6184430
Matteo Piazza <matteo.piazza@gmail.com>
2020-10-29 23:00:58 +0100

    test yarn build

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>wella!</h1>
+    <h1>Dimonni!</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
