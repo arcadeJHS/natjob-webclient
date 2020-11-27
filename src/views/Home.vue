2a4601a359cb12b899272d9c56334a1b3486b078
Matteo Piazza <matteo.piazza@gmail.com>
2020-11-27 17:13:31 +0100

    colors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,34 @@
 <template>
-  <div class="container p-4">
+  <div>
+
+    <div v-if="loading" class="p-4">
+      <svg class="animate-spin h-5 w-5 text-indigo-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+    </div>
 
     <section v-for="source in sources" :key="source.url">
-      <h2 class="text-lg font-semibold text-indigo-500 mb-0.5">
-        <a class="underline text-indigo-500" :href="source.url" target="_BLANK">{{ source.name }}</a>
-        ({{source.results.length}} annunci)
+      <h2 class="text-lg font-semibold bg-indigo-800 text-white p-4">
+        <a class="hover:underline" :href="source.url" target="_BLANK">
+          {{ source.name }}
+          ({{source.results.length}} annunci)
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4 inline">
+            <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+            <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+          </svg>
+        </a>
       </h2>
-      <ul class="divide-y divide-gray-100">
-        <li class="pt-4" v-for="job in source.results" :key="job.url">
+      <ul class="divide-y divide-gray-100 pl-4">
+        <li class="py-2" v-for="job in source.results" :key="job.url">
           <article class="flex">
             <div class="min-w-0 relative flex-auto sm:pr-20 lg:pr-0 xl:pr-20">
-              <a class="underline" :href="job.url" target="_BLANK">
-                <h2 class="text-lg font-semibold text-indigo-500 mb-0.5">
-                  {{ job.title }}
-                  <svg class="h-6 w-6 inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+              <a class="hover:underline" :href="job.url" target="_BLANK">
+                <h2 class="text-indigo-800 mb-0.5 inline">
+                  <span class="font-semibold">{{ job.title }}</span>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4 inline">
+                    <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                    <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
                   </svg>
                 </h2>
               </a>
@@ -22,7 +36,13 @@
                 <div class="flex-none w-full mt-0.5">
                   <dt class="inline mr-1 font-normal">Fonte originale:</dt>
                   <dd class="inline">
-                    <a class="underline text-indigo-500" :href="job.originalSourceJobs" target="_BLANK">{{ job.originalSource }}</a>
+                    <a class="text-indigo-600 hover:underline" :href="job.originalSourceJobs" target="_BLANK">
+                      {{ job.originalSource }}
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-4 w-4 inline">
+                        <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                        <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+                      </svg>
+                    </a>
                     </dd>
                 </div>
                 <div class="flex-none w-full mt-0.5">
@@ -60,16 +80,21 @@ export default {
   name: 'Home',
   data () {
     return {
-      sources: null
+      sources: null,
+      loading: false
     };
   },
   mounted () {
-    // test fetch
     const url = `${process.env.VUE_APP_API_BASE}/jobs`;
     
+    this.loading = true;
+
     fetch(url, {method: 'GET'})
       .then(response => response.json())
-      .then(data => this.sources = data);
+      .then(data => {
+        this.loading = false;
+        this.sources = data;
+      });
   }
 }
 </script>
