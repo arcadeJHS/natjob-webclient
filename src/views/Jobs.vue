e53ac85bb356e1c8597c3382479d25c704dd5afd
Matteo Piazza <matteo.piazza@gmail.com>
2020-12-07 22:13:20 +0100

    koala

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -1,17 +1,17 @@
 <template>
   <Suspense>
 
-    <template #default>
+    <template #fallback>
       <jobs-sources />
     </template>
 
-    <template #fallback>
+    <template #default>
      
       <div class="p-4 text-indigo-600">
         <div class="flex flex-col items-center">
           <p>Ciao Nat, sono il tuo Koala-bot!</p>
           <p>Sto cercando, abbi pazienza!</p>
-          <div class="animate-pulse mt-4">
+          <div class="animate-pulse mt-10">
             <p class="animate-spin text-5xl">&#128040;</p>
           </div>
         </div>
