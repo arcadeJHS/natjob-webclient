29f68ea5c6eb684c80a8e1445787809574c76f7e
Matteo Piazza <matteo.piazza@gmail.com>
2020-12-08 21:12:07 +0100

    layout

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -8,7 +8,7 @@
     <template #fallback>
       <div class="p-4 pt-8 text-indigo-600">
         <div class="flex flex-col items-center">
-          <p>Ciao Nat, sono il tuo Koala-bot!</p>
+          <p>Ciao Nat, sono il tuo koala-bot!</p>
           <div class="animate-pulse my-10">
             <p class="animate-spin text-5xl">&#128040;</p>
           </div>
