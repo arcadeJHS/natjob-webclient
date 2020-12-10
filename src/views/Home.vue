9fccadf455e6e494bb7f873cccee3ad1ec4a9781
Matteo Piazza <matteo.piazza@gmail.com>
2020-12-10 12:19:03 +0100

    layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="flex flex-col items-center p-4">
-    <div class="flex flex-col items-stretch w-full md:flex-row md:w-auto">
-      <form>
-        <input 
-          class="border border-gray-200 rounded p-2 mr-0 mb-2 md:mr-2 md:mb-0 md:w-60"
-          type="text" 
-          v-model="keyword" 
-          placeholder="parola chiave" />
-        <button
-          type="submit" 
-          class="bg-pink-500 text-white border border-transparent hover:bg-white hover:text-pink-500  hover:border-pink-500 px-4 py-2 rounded"
-          @click="search">
-          Cerca
-        </button>
-      </form>
-    </div>
+    <form class="flex flex-col items-stretch w-full md:flex-row md:w-auto">
+      <input 
+        class="border border-gray-200 rounded p-2 mr-0 mb-2 md:mr-2 md:mb-0 md:w-60"
+        type="text" 
+        v-model="keyword" 
+        placeholder="parola chiave" />
+      <button
+        type="submit" 
+        class="bg-pink-500 text-white border border-transparent hover:bg-white hover:text-pink-500  hover:border-pink-500 px-4 py-2 rounded"
+        @click="search">
+        Cerca
+      </button>
+    </form>
   </div>
 </template>
 
